9cba188a5961e52170c539da5f870179acaeb1d5
nikitavoitik <nikitavoitik2006@gmail.com>
2025-02-27 12:50:46 +0100

    Init

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "harbour_swer"
---
+++ src/test/java/com/example/harbour_swer/HarbourSwerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.harbour_swer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarbourSwerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
