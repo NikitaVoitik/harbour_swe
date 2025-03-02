973511f397030c05d6a8d75e4162f14767cdc207
nikitavoitik <nikitavoitik2006@gmail.com>
2025-03-02 11:00:05 +0100

    Added email collection api, using Service class

--- src/main/java/com/example/harbour_swer/forms/Subscription.java
+++ src/main/java/com/example/harbour_swer/forms/Subscription.java
@@ -4,16 +4,31 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 
+
 @Data
 public class Subscription {
+    private enum Source {
+        API,
+        PAGE
+    }
+
     @NotEmpty(message = "Email is required")
     @Email(message = "Email should be valid")
     private String email;
     private String timestamp;
     private String ip;
+    private Source source;
 
     public Subscription() {
         timestamp = java.time.LocalDateTime.now().toString();
+    }
+
+    public void setPageSource() {
+        source = Source.PAGE;
+    }
+
+    public void setApiSource()  {
+        source = Source.API;
     }
 
     public Subscription(String email) {
